a756dd6956db725e070153343152c2d1be9e7f48
Otávio Fernandes <ofernandes@schubergphilis.com>
2019-03-29 13:09:17 +0100

    Bare bones upload.

--- pkg/vault-handler/handler.go
+++ pkg/vault-handler/handler.go
@@ -1,26 +1,21 @@
 package vaulthandler
 
 import (
-	"fmt"
 	"log"
 	"path"
 )
 
 // Handler application primary runtime object.
 type Handler struct {
-	config *Config
-	vault  *Vault
+	config *Config // configuration instance
+	vault  *Vault  // vault api instance
 }
 
 // persist a slice of bytes to file-system.
-func (h *Handler) persist(groupName string, data *SecretData, payload []byte) error {
+func (h *Handler) persist(group string, data *SecretData, payload []byte) error {
 	var err error
 
-	fileName := fmt.Sprintf("%s.%s.%s", groupName, data.Name, data.Extension)
-	filePath := path.Join(h.config.OutputDir, fileName)
-	log.Printf("[Handler] Writting '%s'", filePath)
-
-	file := NewFile(filePath, payload)
+	file := NewFile(group, data, payload)
 
 	if data.Unzip {
 		log.Print("[Handler] Extracting ZIP payload.")
@@ -30,9 +25,10 @@ func (h *Handler) persist(groupName string, data *SecretData, payload []byte) er
 	}
 
 	if h.config.DryRun {
-		log.Printf("[DRY-RUN] File '%s' is not written to file-system!", filePath)
+		log.Printf("[DRY-RUN] File '%s' is not written to file-system!",
+			file.FilePath(h.config.OutputDir))
 	} else {
-		if err = file.Write(); err != nil {
+		if err = file.Write(h.config.OutputDir); err != nil {
 			return err
 		}
 	}
@@ -40,8 +36,9 @@ func (h *Handler) persist(groupName string, data *SecretData, payload []byte) er
 	return nil
 }
 
-// Run app main loop.
-func (h *Handler) Run(manifest *Manifest) error {
+// Authenticate against vault either via token directly or via AppRole, must be invoked before other
+// actions using the API.
+func (h *Handler) Authenticate() error {
 	var err error
 
 	if h.config.VaultToken != "" {
@@ -54,20 +51,31 @@ func (h *Handler) Run(manifest *Manifest) error {
 		}
 	}
 
-	for groupName, secrets := range manifest.Secrets {
-		log.Printf("[Handler] Handling secrets for '%s' group...", groupName)
-		log.Printf("[Handler] [%s] Vault path '%s'", groupName, secrets.Path)
+	return nil
+}
+
+// composeVaultPath based in the current SecretData.
+func (h *Handler) composeVaultPath(secrets Secrets, data SecretData) string {
+	if !data.NameAsSubPath {
+		return secrets.Path
+	}
+	return path.Join(secrets.Path, data.Name)
+}
+
+// Download files from vault based on manifest.
+func (h *Handler) Download(manifest *Manifest) error {
+	var err error
+
+	for group, secrets := range manifest.Secrets {
+		log.Printf("[Handler/Download] Handling secrets for '%s' group...", group)
+		log.Printf("[Handler/Download] [%s] Vault path '%s'", group, secrets.Path)
 
 		for _, data := range secrets.Data {
-			log.Printf("[Handler] [%s] Reading data from Vault '%s.%s' (unzip: %v)",
-				groupName, data.Name, data.Extension, data.Unzip)
+			log.Printf("[Handler/Download] [%s] Reading data from Vault '%s.%s' (unzip: %v)",
+				group, data.Name, data.Extension, data.Unzip)
 
-			// putting together the secret path in vault
-			vaultPath := secrets.Path
-			if data.NameAsSubPath {
-				vaultPath = path.Join(vaultPath, data.Name)
-			}
-			log.Printf("[Handler] [%s] '%s' path in Vault: '%s'", data.Name, groupName, vaultPath)
+			vaultPath := h.composeVaultPath(secrets, data)
+			log.Printf("[Handler/Download] [%s] '%s' path in Vault: '%s'", data.Name, group, vaultPath)
 
 			// loading secret from vault
 			payload := []byte{}
@@ -76,10 +84,62 @@ func (h *Handler) Run(manifest *Manifest) error {
 			}
 
 			// saving data to disk
-			if err = h.persist(groupName, &data, payload); err != nil {
+			if err = h.persist(group, &data, payload); err != nil {
 				return err
 			}
 		}
+	}
+
+	return nil
+}
+
+// Upload files to Vault, accordingly to the manifest.
+func (h *Handler) Upload(manifest *Manifest) error {
+	var err error
+
+	log.Printf("[Handler/Upload] Input directory: '%s'", h.config.InputDir)
+
+	for group, secrets := range manifest.Secrets {
+		log.Printf("[Handler/Upload] Handling secrets for '%s' group...", group)
+		log.Printf("[Handler/Upload] [%s] Vault path '%s'", group, secrets.Path)
+
+		for _, data := range secrets.Data {
+			file := NewFile(group, &data, []byte{})
+
+			if err = file.Read(h.config.InputDir); err != nil {
+				log.Printf("[Handler/Upload] error on reading file: '%s'", err)
+				return err
+			}
+
+			if data.Unzip {
+				if err = file.Zip(); err != nil {
+					log.Printf("[Handler/Upload] error on zipping file payload: '%s'", err)
+					return err
+				}
+			}
+
+			if err = h.dispense(file, h.composeVaultPath(secrets, data)); err != nil {
+				log.Printf("[Handler/Upload] Error on writting data to Vault: '%s'", err)
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (h *Handler) dispense(file *File, vaultPath string) error {
+	var data = make(map[string]interface{})
+	var err error
+
+	if h.config.DryRun {
+		log.Printf("[DRY-RUN] File '%s' is not uploaded to Vault path '%s'", file.Name(), vaultPath)
+		return nil
+	}
+
+	data[file.Name()] = file.Payload()
+	if err = h.vault.Write(vaultPath, data); err != nil {
+		return err
 	}
 
 	return nil

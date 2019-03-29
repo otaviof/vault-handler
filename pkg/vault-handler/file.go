031b2ed6874f20911e3654890f0a0afb84a90a54
Otávio Fernandes <ofernandes@schubergphilis.com>
2019-03-29 12:21:11 +0100

    Being able to write and zip/unzip payload.

--- pkg/vault-handler/file.go
+++ pkg/vault-handler/file.go
@@ -3,14 +3,40 @@ package vaulthandler
 import (
 	"bytes"
 	"compress/gzip"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
+	"path"
 )
 
 // File definition on how to write a secret to file-system.
 type File struct {
-	path    string // full path, including name and extension
-	payload []byte // data payload
+	group      string      // name of the group
+	properties *SecretData // using SecretData as file properties
+	payload    []byte      // data payload
+}
+
+// Zip file payload with gzip.
+func (f *File) Zip() error {
+	var buffer bytes.Buffer
+	var err error
+
+	gz := gzip.NewWriter(&buffer)
+
+	if _, err = gz.Write(f.payload); err != nil {
+		return err
+	}
+	if err = gz.Flush(); err != nil {
+		return err
+
+	}
+	if err = gz.Close(); err != nil {
+		return err
+	}
+
+	f.payload = buffer.Bytes()
+	return nil
 }
 
 // Unzip payload.
@@ -23,7 +49,6 @@ func (f *File) Unzip() error {
 	if reader, err = gzip.NewReader(bufferIn); err != nil {
 		return err
 	}
-
 	if _, err = bufferOut.ReadFrom(reader); err != nil {
 		return err
 	}
@@ -32,12 +57,43 @@ func (f *File) Unzip() error {
 	return nil
 }
 
+// Read payload from file-system.
+func (f *File) Read(baseDir string) error {
+	var err error
+
+	fullPath := f.FilePath(baseDir)
+	if !fileExists(fullPath) {
+		return fmt.Errorf("can't find file '%s'", fullPath)
+	}
+	if f.payload, err = ioutil.ReadFile(fullPath); err != nil {
+		return err
+	}
+	log.Printf("[File] Reading '%d' bytes from '%s'", len(f.payload), fullPath)
+
+	return nil
+}
+
 // Write contents to file-system.
-func (f *File) Write() error {
-	return ioutil.WriteFile(f.path, f.payload, 0600)
+func (f *File) Write(baseDir string) error {
+	return ioutil.WriteFile(f.FilePath(baseDir), f.payload, 0600)
+}
+
+// fileName compose file name based on group and SecretData settings.
+func (f *File) fileName() string {
+	return fmt.Sprintf("%s.%s.%s", f.group, f.properties.Name, f.properties.Extension)
+}
+
+// FilePath joins the infomed base directory with file name.
+func (f *File) FilePath(baseDir string) string {
+	return path.Join(baseDir, f.fileName())
+}
+
+// Payload returns the file payload as slice of bytes.
+func (f *File) Payload() []byte {
+	return f.payload
 }
 
 // NewFile instance.
-func NewFile(path string, payload []byte) *File {
-	return &File{path: path, payload: payload}
+func NewFile(group string, properties *SecretData, payload []byte) *File {
+	return &File{group: group, properties: properties, payload: payload}
 }
--- pkg/vault-handler/file_test.go
+++ pkg/vault-handler/file_test.go
@@ -1,8 +1,6 @@
 package vaulthandler
 
 import (
-	"bytes"
-	"compress/gzip"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,38 +8,24 @@ import (
 
 var file *File
 
-func zipBytes(payload []byte) ([]byte, error) {
-	var buffer bytes.Buffer
-	var err error
-
-	gz := gzip.NewWriter(&buffer)
-
-	if _, err = gz.Write(payload); err != nil {
-		return nil, err
-	}
-	if err = gz.Flush(); err != nil {
-		return nil, err
-	}
-	if err = gz.Close(); err != nil {
-		return nil, err
-	}
-
-	return buffer.Bytes(), nil
-}
+const payload = "payload"
 
 func TestFileNewFile(t *testing.T) {
-	zipped, err := zipBytes([]byte("payload"))
-	assert.Nil(t, err)
-	assert.NotNil(t, zipped)
-	assert.True(t, len(zipped) > 0)
+	secretData := &SecretData{Name: "file", Extension: "text"}
+	file = NewFile("test", secretData, []byte(payload))
+	assert.True(t, len(file.payload) > 0)
+}
 
-	file = NewFile("/var/tmp/vault-handler.test", zipped)
-	assert.Equal(t, zipped, file.payload)
+func TestFileZip(t *testing.T) {
+	err := file.Zip()
+
+	assert.Nil(t, err)
+	assert.True(t, len(file.payload) > 0)
 }
 
 func TestFileUnzip(t *testing.T) {
 	err := file.Unzip()
 
 	assert.Nil(t, err)
-	assert.Equal(t, []byte("payload"), file.payload)
+	assert.Equal(t, []byte(payload), file.payload)
 }
